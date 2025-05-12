6695517a1a5d12d42cc52273a29e05e3e93dab31
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-12 22:10:38 +0700

    write post mapping in controller for createPatient action

--- src/main/java/com/pm/patient_service/controller/PatientController.java
+++ src/main/java/com/pm/patient_service/controller/PatientController.java
@@ -1,10 +1,14 @@
 package com.pm.patient_service.controller;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.service.PatientService;
+import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +27,13 @@ public class PatientController {
     List<PatientResponseDTO> patients = patientService.getPatients();
 
     return ResponseEntity.ok().body(patients);
+  }
+
+  @PostMapping
+  public ResponseEntity<PatientResponseDTO> createPatient(
+      @Valid @RequestBody PatientRequestDTO patientRequestDTO) {
+    PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+
+    return ResponseEntity.ok().body(patientResponseDTO);
   }
 }
