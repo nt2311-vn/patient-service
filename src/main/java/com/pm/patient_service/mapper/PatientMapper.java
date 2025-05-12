977d51fba7be092e096d201d56258f7489276d2c
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-12 22:06:12 +0700

    write mapper to turn patientRequestDTO to PatientModel

--- src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,9 @@
 package com.pm.patient_service.mapper;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -15,5 +17,17 @@ public class PatientMapper {
     patientDTO.setDateOfBirth(patient.getDateOfBirth().toString());
 
     return patientDTO;
+  }
+
+  public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+    Patient patient = new Patient();
+
+    patient.setName(patientRequestDTO.getName());
+    patient.setEmail(patientRequestDTO.getEmail());
+    patient.setAddress(patient.getAddress());
+    patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+    patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+
+    return patient;
   }
 }
