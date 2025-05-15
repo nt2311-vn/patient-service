664cf10146adb40e8ff87eb3fb30de64dc51fef5
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-15 22:20:54 +0700

    update patient service on new exception email already exists

--- src/main/java/com/pm/patient_service/service/PatientService.java
+++ src/main/java/com/pm/patient_service/service/PatientService.java
@@ -2,6 +2,7 @@ package com.pm.patient_service.service;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -24,6 +25,10 @@ public class PatientService {
   }
 
   public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+    if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+      throw new EmailAlreadyExistsException(
+          "A patient with this email already exists" + patientRequestDTO.getEmail());
+    }
     Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
     return PatientMapper.toDTO(newPatient);
   }
