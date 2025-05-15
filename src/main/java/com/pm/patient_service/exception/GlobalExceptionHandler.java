6dd13082806098cb6e48b9ca948c92b8dbab7d76
nt2311-vn <nguyentoan231196@gmail.com>
2025-05-15 21:10:10 +0700

    global handler exception more insightful

---
+++ src/main/java/com/pm/patient_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pm.patient_service.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+/** GlobalExceptionHandler */
+public class GlobalExceptionHandler {
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationException(
+      MethodArgumentNotValidException ex) {
+
+    Map<String, String> errors = new HashMap<>();
+
+    ex.getBindingResult()
+        .getFieldErrors()
+        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+    return ResponseEntity.badRequest().body(errors);
+  }
+}
